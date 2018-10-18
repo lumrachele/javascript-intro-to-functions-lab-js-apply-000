fe4bd10cfe7e674d44e3c4479b6154dc31128ff5
Rachel Lum <rlum894@gmail.com>
2018-10-18 18:29:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function whisper(string){
 }
 
 function logShout(string){
+<<<<<<< HEAD
   console.log(`${string.toUpperCase()}`)
 }
 
@@ -40,3 +41,21 @@ function sayHiToGrandma(string) {
     return "I love you, too."
   }
 }
+=======
+  console.log(`${string}.toUpperCase()`)
+}
+
+function logWhisper(string){
+  console.log(`${string}.toLowerCase()`)
+}
+
+function sayHiToGrandma (string){
+  if (string === lowercase)
+    return "I can't hear you!"
+  else if (string === uppercase)
+    return "YES INDEED!"
+  else if (string === "I love you, Grandma")
+  return "I love you, too."
+  
+}
+>>>>>>> 32c7faccba0c9f028852126419cb6a2acd0776f6
